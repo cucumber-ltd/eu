50c1228a0702640c786831412c08df618bdf6134
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2014-05-22 22:48:32 +0100

    Added a null cache

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = require('./lib/eu');
 module.exports.Cache = require('./lib/cache');
+module.exports.NullCache = require('./lib/null_cache');
 module.exports.MemoryStore = require('./lib/memory_store');
 module.exports.RedisStore = require('./lib/redis_store');
